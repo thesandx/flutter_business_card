87ba6c144712aaac2cd14fac693eea1fa73ac836
thesandx <kumarjha.sandeep@gmail.com>
2020-04-23 15:41:48 +0530

    flutter card part 1-adding fonts and image assests.

--- lib/main.dart
+++ lib/main.dart
@@ -11,34 +11,38 @@ class MyApp extends StatelessWidget {
         home: Scaffold(
           backgroundColor: Colors.blueGrey,
           body: SafeArea(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Container(
-                  width: 100.0,
-                  color: Colors.red,
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/sandy.jpeg'),
                 ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      width: 100.0,
-                      height:100.0,
-                      color: Colors.yellow,
-                    ),
-                    Container(
-                      width: 100.0,
-                      height:100.0,
-                      color: Colors.green,
-                    ),
-                  ],
+                Text(
+                    "Sandeep Kumar Jha",
+                    style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight:FontWeight.bold,
+                    color: Colors.white,
+                    fontFamily: 'Pacifico'
+                  ),
                 ),
-                Container(
-                  width: 100.0,
-                  color: Colors.blue,
+                SizedBox(
+                  height: 2.0,
+                ),
+
+                Text(
+                    "SOFTWARE DEVELOPER",
+                    style: TextStyle(
+                        fontSize: 12.0,
+                        fontWeight:FontWeight.bold,
+                        color: Colors.teal.shade100,
+                        fontFamily: 'SourceSansPro',
+                        letterSpacing:2.5
+                    ),
                 )
               ],
-            ),
+            )
           ),
     ),
     );
